cb661fcff54eca8a76cad3c55b89745fe30d7f31
matrakugeri <matrakugeri@gmail.com>
2025-12-29 23:07:28 +0100

    define initial game state variables

--- src/app/pages/tic-tac-toe/tic-tac-toe.ts
+++ src/app/pages/tic-tac-toe/tic-tac-toe.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-tic-tac-toe',
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './tic-tac-toe.scss',
 })
 export class TicTacToe {
-
+  board = signal<(string | null)[]>(Array(9).fill(null));
+  turn = signal<'X' | 'O'>('X');
+  player = signal<number>(1);
+  winner = signal<string | null>(null);
+  winningCells = signal<number[]>([]);
+  readonly winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 }
