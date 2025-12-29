bdb296394ff45417e290365a876232d0a374f93d
matrakugeri <matrakugeri@gmail.com>
2025-12-29 22:49:20 +0100

    defined app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/tic-tac-toe/tic-tac-toe').then((m) => m.TicTacToe),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/tic-tac-toe/tic-tac-toe').then((m) => m.TicTacToe),
+  },
+  {
+    path: 'TicTacToe',
+    loadComponent: () => import('./pages/tic-tac-toe/tic-tac-toe').then((m) => m.TicTacToe),
+  },
+];
