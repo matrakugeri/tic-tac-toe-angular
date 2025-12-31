c35ab41bb1283d829ae32704b4e2add6ba5bd8d7
matrakugeri <matrakugeri@gmail.com>
2025-12-31 23:58:23 +0100

    fix route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'TicTacToe',
+    loadComponent: () => import('./pages/tic-tac-toe/tic-tac-toe').then((m) => m.TicTacToe),
   },
   {
     path: '**',
-    redirectTo: 'TicTacToe',
-  },
-  {
-    path: 'TicTacToe',
     loadComponent: () => import('./pages/tic-tac-toe/tic-tac-toe').then((m) => m.TicTacToe),
   },
 ];
