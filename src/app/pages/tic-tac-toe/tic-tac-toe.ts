98925cfc8613c18d9f0dc63fc8c4db0916a1cea2
matrakugeri <matrakugeri@gmail.com>
2025-12-31 18:20:47 +0100

    implement check winner logic

--- src/app/pages/tic-tac-toe/tic-tac-toe.ts
+++ src/app/pages/tic-tac-toe/tic-tac-toe.ts
@@ -25,11 +25,29 @@ export class TicTacToe {
 
   play(index: number): void {
     if (this.board()[index] || this.winner()) return;
+
     this.board.update((oldBoard) => {
       const updatedBoard = [...oldBoard];
       updatedBoard[index] = this.turn();
       console.log(updatedBoard);
       return updatedBoard;
     });
+    this.checkWinner();
+    this.player.set(this.turn() === 'X' ? 2 : 1);
+    this.turn.set(this.turn() === 'X' ? 'O' : 'X');
+  }
+
+  checkWinner() {
+    for (const combo of this.winningCombos) {
+      const [a, b, c] = combo;
+      if (
+        this.board()[a] &&
+        this.board()[a] === this.board()[b] &&
+        this.board()[b] === this.board()[c]
+      ) {
+        this.winner.set(this.turn());
+        this.winningCells.set(combo);
+      }
+    }
   }
 }
